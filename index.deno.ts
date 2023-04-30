d6d08593c2e79fec72bda1c98d240007477e6b81
Zander Hill <zander@xargs.io>
2023-04-30 10:49:25 -0700

    Add caching

--- index.deno.ts
+++ index.deno.ts
@@ -28,22 +28,49 @@ const getSheetData = async (url:string)   => {
   return output
 }
 
+const CACHE_KEY = ['DATA', 'GOOGLE_SHEET_DATA']
+const CACHE_KEY_EXPIRES_AT = ['DATA', 'GOOGLE_SHEET_EXPIRES_AT']
+const TTL_MILLIS = 30 * 60 * 1000
+
 const handler = async (req: Request) => {
-
-  const data = await getSheetData(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${SHEET_NAME}!A1:D100000?key=${GOOGLE_SHEET_TOKEN}`)
-
   const {url} = req
   const d = new URL(url)
   const path = _.trimStart(d.pathname, '/')
   console.log({path})
+
+  const kv = await Deno.openKv();
+  const ttlResult: Deno.KvEntryMaybe<unknown> = await kv.get(CACHE_KEY_EXPIRES_AT)
+  const now = (new Date()).getTime()
+  if(ttlResult?.value < now) {
+    await kv.delete(CACHE_KEY_EXPIRES_AT)
+    await kv.delete(CACHE_KEY)
+  }
+  const result = await kv.get(CACHE_KEY)
+  let data: Shortlink[] = []
+  if(result.value) {
+    data = result.value as Shortlink[]
+  } else {
+    data = await getSheetData(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${SHEET_NAME}!A1:D100000?key=${GOOGLE_SHEET_TOKEN}`)
+    await kv.atomic()
+      .set(CACHE_KEY, data)
+      .set(CACHE_KEY_EXPIRES_AT, now + TTL_MILLIS)
+      .commit()
+  }
+
+
+  // TODO: set headers indicating whether it was a cache hit and when the cache expires
   const match = data.find(d => d.shortcut === path)
   console.log({match})
   if(match){
     const response_code = match.response_code || 302
     if(response_code >= 300 && response_code < 400) {
-      return Response.redirect(match.url, response_code)
+      let r = Response.redirect(match.url, response_code)
+      //r.headers.set('x-cache-expires-at', )
+      return r
     }
-    return new Response("", {status: response_code})
+    return new Response("", {status: response_code, headers: {
+
+    }})
   }
   return new Response("", {status: 404})
 }
